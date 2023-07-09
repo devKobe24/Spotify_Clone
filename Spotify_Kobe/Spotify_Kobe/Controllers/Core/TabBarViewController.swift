bfbf425a945ad54a531ea86f176ebb0b4140656a
Kobe <mskang@xingxingmobility.com>
2023-07-09 15:50:46 +0900

    :recycle: Refactor: setViewControllers 메서드 구현.
    UITabBarController의 setViewControllers 메서드를 구현하기 위한 요소들인 뷰컨트롤러 인스턴스들과 뷰컨트롤러 타이틀, 네비게이션 컨트롤러 인스턴스를 생성하였으며 그 요소들을 활용하여 setViewController 메서드를 구현하였습니다.

--- Spotify_Kobe/Spotify_Kobe/Controllers/Core/TabBarViewController.swift
+++ Spotify_Kobe/Spotify_Kobe/Controllers/Core/TabBarViewController.swift
@@ -10,5 +10,27 @@ import UIKit
 class TabBarViewController: UITabBarController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		let homeViewController = HomeViewController()
+		let searchViewController = SearchViewController()
+		let libraryViewController = LibraryViewController()
+		
+		homeViewController.title = "Home"
+		searchViewController.title = "Search"
+		libraryViewController.title = "Library"
+		
+		homeViewController.navigationItem.largeTitleDisplayMode = .always
+		searchViewController.navigationItem.largeTitleDisplayMode = .always
+		libraryViewController.navigationItem.largeTitleDisplayMode = .always
+		
+		let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+		let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+		let libraryNavigationController = UINavigationController(rootViewController: libraryViewController)
+		
+		homeNavigationController.navigationBar.prefersLargeTitles = true
+		searchNavigationController.navigationBar.prefersLargeTitles = true
+		libraryNavigationController.navigationBar.prefersLargeTitles = true
+		
+		setViewControllers([homeNavigationController, searchNavigationController, libraryNavigationController], animated: false)
 	}
 }
