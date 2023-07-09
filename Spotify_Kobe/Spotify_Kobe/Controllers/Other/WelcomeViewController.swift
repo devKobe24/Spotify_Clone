cd51d0ef5460e575143814bf157e0d48d0d0d0a0
Kobe <mskang@xingxingmobility.com>
2023-07-09 15:20:13 +0900

    :sparkles: Feat: WelcomeViewController 생성.
    Controllers 폴더 하위에 Other 폴더를 생성하고 그 내부에 WelcomeViewController를 생성하였습니다.

---
+++ Spotify_Kobe/Spotify_Kobe/Controllers/Other/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  Spotify_Kobe
+//
+//  Created by Minseong Kang on 2023/07/09.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
