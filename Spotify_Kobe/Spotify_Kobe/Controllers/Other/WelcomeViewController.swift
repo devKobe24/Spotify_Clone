1fa37bc7233bc4e8376ee88d537fa1a3c421cb42
Kobe <mskang@xingxingmobility.com>
2023-07-21 15:49:27 +0900

    :recycle: Refactor: viewDidLayoutSubviews 오버라이드, didTapSingInButton 메서드 구현.
    viewDidLayoutSubviews 오버라이드, 버튼 액션 메서드인 didTapSingInButton 메서드 생성 및 구현.

--- Spotify_Kobe/Spotify_Kobe/Controllers/Other/WelcomeViewController.swift
+++ Spotify_Kobe/Spotify_Kobe/Controllers/Other/WelcomeViewController.swift
@@ -14,5 +14,19 @@ class WelcomeViewController: UIViewController {
 		self.title = "Spotify"
 		self.view.backgroundColor = .systemGreen
     }
+	
+	// MARK: - viewDidLayoutSubviews
+	/// 뷰가 방금 하위 뷰를 배치했음을 뷰 컨트롤러에 알리기 위해 호출됩니다.
+	/// 뷰 컨트롤러의 뷰에 대한 범위가 변경되면 뷰는 하위 뷰의 위치를 조정한 다음 시스템에서 이 메서드를 호출합니다.
+	/// 참고 문서 : https://developer.apple.com/documentation/uikit/uiviewcontroller/1621398-viewdidlayoutsubviews
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+	}
+	
+	@objc func didTapSingInButton() {
+		let authViewController = AuthViewController()
+		authViewController.navigationItem.largeTitleDisplayMode = .never
+		navigationController?.pushViewController(authViewController, animated: true)
+	}
 
 }
