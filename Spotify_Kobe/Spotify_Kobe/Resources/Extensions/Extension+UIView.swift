29d2ca7d3746fc43e217b868759224bc2dd1933a
Kobe <mskang@xingxingmobility.com>
2023-07-21 16:24:15 +0900

    :sparkles: Feat: UIView extension 생성 및 구현.

---
+++ Spotify_Kobe/Spotify_Kobe/Resources/Extensions/Extension+UIView.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Spotify_Kobe
+//
+//  Created by Minseong Kang on 2023/07/21.
+//
+
+import UIKit
+
+extension UIView {
+	var width: CGFloat {
+		return frame.size.width
+	}
+	
+	var height: CGFloat {
+		return frame.size.height
+	}
+	
+	var left: CGFloat {
+		return frame.origin.x
+	}
+	
+	var right: CGFloat {
+		return left + width
+	}
+	
+	var top: CGFloat {
+		return frame.origin.y
+	}
+	
+	var bottom: CGFloat {
+		return top + height
+	}
+}
