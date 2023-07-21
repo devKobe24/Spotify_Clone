46c6ed57957c97f3ef5bbb3ced84702e156b982d
Kobe <mskang@xingxingmobility.com>
2023-07-21 15:45:25 +0900

    :recycle: Refactor: WKNavigationDelegate 채택, viewDidLayoutSubviews 오버라이드.
    WKNavigationDelegate를 채택하고 navigationDelegate를 AuthViewController로 설정, viewDidLayoutSubviews 오버라이드 후 내부에 webView 크기 설정.

--- Spotify_Kobe/Spotify_Kobe/Controllers/Other/AuthViewController.swift
+++ Spotify_Kobe/Spotify_Kobe/Controllers/Other/AuthViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, WKNavigationDelegate {
 
 	private let webView: WKWebView = {
 		let webPagePreferences = WKWebpagePreferences()
@@ -24,6 +24,17 @@ class AuthViewController: UIViewController {
         super.viewDidLoad()
         title = "Sign In"
 		view.backgroundColor = .systemBackground
+		webView.navigationDelegate = self
+		view.addSubview(webView)
     }
+	
+	// MARK: - viewDidLayoutSubviews
+	/// 뷰가 방금 하위 뷰를 배치했음을 뷰 컨트롤러에 알리기 위해 호출됩니다.
+	/// 뷰 컨트롤러의 뷰에 대한 범위가 변경되면 뷰는 하위 뷰의 위치를 조정한 다음 시스템에서 이 메서드를 호출합니다.
+	/// 참고 문서 : https://developer.apple.com/documentation/uikit/uiviewcontroller/1621398-viewdidlayoutsubviews
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		webView.frame = view.bounds
+	}
 
 }
