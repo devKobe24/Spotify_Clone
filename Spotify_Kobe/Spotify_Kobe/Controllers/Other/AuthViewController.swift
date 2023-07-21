ad6dab922ce4f1acb09876a5be4ce73db208a3a7
Kobe <mskang@xingxingmobility.com>
2023-07-21 15:35:47 +0900

    :sparkles: Feat: import WebKit, webView 생성 및 구현.
    WebKit을 import 했으며, webView를 클로저 형태로 기본 구현을 했습니다.

--- Spotify_Kobe/Spotify_Kobe/Controllers/Other/AuthViewController.swift
+++ Spotify_Kobe/Spotify_Kobe/Controllers/Other/AuthViewController.swift
@@ -6,24 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AuthViewController: UIViewController {
 
+	private let webView: WKWebView = {
+		let webPagePreferences = WKWebpagePreferences()
+		webPagePreferences.allowsContentJavaScript = true
+		let webViewConfiguration = WKWebViewConfiguration()
+		webViewConfiguration.defaultWebpagePreferences = webPagePreferences
+		let webView = WKWebView(frame: .zero,
+								configuration: webViewConfiguration)
+		return webView
+	}()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Sign In"
+		view.backgroundColor = .systemBackground
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
